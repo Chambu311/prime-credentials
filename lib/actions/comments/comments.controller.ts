a43c1d2b74399844d91de99a4307d4dd176e96df
Chambu311 <achambu@gmail.com>
2025-03-02 15:39:03 -0300

    removed unused actions

--- lib/actions/comments/comments.controller.ts
+++ lib/actions/comments/comments.controller.ts
@@ -22,20 +22,4 @@ export async function createComment(formData: FormData, postId: string) {
     };
   }
 }
-export async function getCommentsByPostId(postId: string) {
-  try {
-    const supabase = await createClient();
-    const commentsService = new CommentsService(supabase);
-    const data = await commentsService.getCommentsByPostId(postId);
-    return {
-      data: data,
-      error: null,
-    };
-  } catch (error) {
-    console.error(error);
-    return {
-      error: error,
-      data: null,
-    };
-  }
-}
+
