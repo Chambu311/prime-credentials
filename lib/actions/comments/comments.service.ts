1b0d79df8c61619158af68f30cd1c9b38915301a
Chambu311 <achambu@gmail.com>
2025-03-03 10:56:47 -0300

    fixed unsaved cursor change

--- lib/actions/comments/comments.service.ts
+++ lib/actions/comments/comments.service.ts
@@ -2,39 +2,18 @@ import { SupabaseClient } from "@supabase/supabase-js";
 import { ImagesService } from "../images.service";
 
 export class CommentsService {
-  private imagesService: ImagesService;
-
-  constructor(private supabase: SupabaseClient) {
+  private supabase: SupabaseClient;
+  private imageService: ImagesService;
+  constructor(supabase: SupabaseClient) {
     this.supabase = supabase;
-    this.imagesService = new ImagesService(supabase);
+    this.imageService = new ImagesService(supabase);
   }
 
-  async createComment(content: string, postId: string) {
-    console.log(content, postId);
+  async createComment(content: string, postId: string, image: File | null) {
     const {
       data: { user },
     } = await this.supabase.auth.getUser();
-    const { data, error } = await this.supabase.from("comment").insert({
-      content: content,
-      post_id: postId,
-      user_id: user?.id,
-      user_email: user?.email,
-    });
-    if (error) {
-      console.error(error.message);
-      throw new Error(error.message);
-    }
-    return data;
-  }
-
-  async createCommentWithImage(content: string, postId: string, image: File | null) {
-    const {
-      data: { user },
-    } = await this.supabase.auth.getUser();
-    
-    // Upload image if provided
-    const imagePath = image ? await this.imagesService.uploadImageToBucket(image) : null;
-    
+    const imagePath = image ? await this.imageService.uploadImageToBucket(image) : null;
     const { data, error } = await this.supabase.from("comment").insert({
       content: content,
       post_id: postId,
@@ -42,31 +21,10 @@ export class CommentsService {
       user_email: user?.email,
       image: imagePath,
     });
-    
     if (error) {
       console.error(error.message);
       throw new Error(error.message);
     }
-    return data;
-  }
-
-  async getCommentsByPostId(postId: string) {
-    const { data, error } = await this.supabase
-      .from("comment")
-      .select("*")
-      .eq("post_id", postId)
-      .order("created_at", { ascending: false });
-    if (error) {
-      throw new Error(error.message);
-    }
-    
-    // Get signed URLs for any comment images
-    for (const comment of data) {
-      if (comment.image) {
-        comment.image = await this.imagesService.getPostImageSignedUrl(comment.image);
-      }
-    }
-    
     return data;
   }
 }
